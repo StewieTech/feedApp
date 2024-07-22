cce0d2c59c3af965bd92fcad77185460df02e8ea
flystewie <77213085+flystewie@users.noreply.github.com>
2024-07-22 13:19:45 -0400

    Update video

---
+++ src/main/java/com/feedApp/FeedAppApplication.java
@@ -0,0 +1,12 @@
+package com.feedApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedAppApplication.class, args);
+}
+}
