260c083773bb95fafab944c5d0c6b823292a8d63
flystewie <77213085+flystewie@users.noreply.github.com>
2024-07-23 11:52:10 -0400

    Update UserService for JPA

--- src/main/java/com/feedApp/controller/UserController.java
+++ src/main/java/com/feedApp/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.feedApp.controller;
 
 import com.feedApp.jdbc.UserBean;
-import com.feedApp.jdbc.UserService;
+import com.feedApp.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
---
+++ src/main/java/com/feedApp/service/UserService.java
@@ -0,0 +1,29 @@
+package com.feedApp.service;
+
+import com.feedApp.jpa.User;
+import com.feedApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service ;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> listUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return this.userRepository.findByUsername(username);
+    }
+
+    public void createUser(User user) {
+        this.userRepository.save(user);
+    }
+}
+
