d93c3d4ec98c82247ab7cb8f5e6d18e913dc6647
flystewie <77213085+flystewie@users.noreply.github.com>
2024-07-23 11:37:47 -0400

    Add JPA and userRepository interface

---
+++ src/main/java/com/feedApp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.feedApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.feedApp.jpa.User ;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmailId(String email) ;
+    
+}
