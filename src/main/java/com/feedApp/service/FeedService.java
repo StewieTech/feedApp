de3d4899f4571e32922e0aaebd338c5229217420
flystewie <77213085+flystewie@users.noreply.github.com>
2024-07-31 14:45:56 -0400

    Add deleteFeed to FeedService

--- src/main/java/com/feedApp/service/FeedService.java
+++ src/main/java/com/feedApp/service/FeedService.java
@@ -113,4 +113,16 @@ public class FeedService {
         return this.feedMetaDataRepository.save(newMeta) ;
     }
 
+    public void deleteFeed(int feedId) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Feed feed = this.feedRepository.findById(feedId)
+                .orElseThrow(() -> new FeedNotFoundException(String.format("Feed doesn't exist %d", feedId)));
+
+        Optional.of(feed).filter(f -> f.getUser().getUsername().equals(username))
+                .orElseThrow(()-> new FeedNotFoundException(String.format("Feed doesn't belong to current User, feedId: %d, username: %s", feedId, username)));
+
+        this.feedRepository.delete(feed);
+    }
+
 }
